d5055ae00828a9be2de9a4023be6d858ef1d4f50
HiGoldAnimationStudios <higoldanimationstudios@gmail.com>
2025-12-12 16:10:26 -0500

    more graphs with orion help

--- initialize_balls_plot.m
+++ initialize_balls_plot.m
@@ -1,5 +1,5 @@
 function ball_plot_struct = initialize_balls_plot() 
     ball_plot_struct = struct();
-    ball_plot_struct.line_plot=plot(0,0,'k','linewidth',2);
-    ball_plot_struct.point_plot=plot(0,0,'ro','markerfacecolor','r','markersize',7);
+    ball_plot_struct.line_plot=plot(0,0,'k','linewidth',2, "DisplayName","String");
+    ball_plot_struct.point_plot=plot(0,0,'ro','markerfacecolor','r','markersize',3, "DisplayName","Masses");
 end
--- update_balls_plot.m
+++ update_balls_plot.m
@@ -1,10 +1,13 @@
 function update_balls_plot(ball_plot_struct,V,t,string_params)
-    % X points
-    x_points=[0];
-    for i=1:string_params.n
-        x_points(end+1)=string_params.dx*i;
-    end
-    x_points(end+1)=string_params.L;
+    % % X points
+    % x_points=[0];
+    % for i=1:string_params.n
+    %     x_points(end+1)=string_params.dx*i;
+    % end
+    % x_points(end+1)=string_params.L;
+
+
+    x_points = linspace(0,string_params.L,string_params.n+2);
 
     % Y points
     l=length(V)/2;
